78907da10395737192deb069cc6d0bd7325cdc1a
DESKTOP-83H84IH\Ugo <ugo.marchesini@libero.it>
2018-04-21 16:39:54 +0200

    Commit 21 04 2018 - gestione poligonale

--- GeometriaSolution/Geometria/Definizioni.cs
+++ GeometriaSolution/Geometria/Definizioni.cs
@@ -12,22 +12,40 @@ namespace Geometria
         {
             private List<Double> _Dimensions = new List<Double>();
 
+            private String _Hash = String.Empty;
+
             public Punto(Double x , Double y)
             {
                 _Dimensions.Add(x);
                 _Dimensions.Add(y);
+                GetHash();
             }
 
             public Double X
             {
                 get { return this._Dimensions[0]; }
-                set { this._Dimensions[0] = value; }
+                set { this._Dimensions[0] = value; GetHash(); }
             }
 
             public Double Y
             {
                 get { return this._Dimensions[1]; }
-                set { this._Dimensions[1] = value; }
+                set { this._Dimensions[1] = value; GetHash(); }
+            }
+
+            private void  GetHash()
+            {
+                _Hash = GetHash(this); 
+            }
+
+            public static String GetHash(Punto punto)
+            {
+                return String.Format("{0}-{1}",punto.X , punto.Y); 
+            }
+
+            public String Hash
+            {
+                get { return this._Hash; }
             }
         }
 
@@ -54,17 +72,123 @@ namespace Geometria
 
         public class Segmento
         {
+            private Double _Lunghezza = 0;
+
             public Segmento(Punto a, Punto b)
             {
 
+            }
+
+            public Double Lunghezza
+            {
+                get { return this._Lunghezza; }
+            }
+        }
+
+
+        public class Vettore : Segmento
+        {
+            public Vettore(Punto a, Punto b) : base(a, b)
+            {
             }
         }
 
         public class Poligonale
         {
-            public Poligonale(List<Segmento> lineaSpezzata)
+            protected List<Punto> _Punti = new List<Punto>();
+            protected List<Segmento> _LineaSpezzata = new List<Segmento>();
+            protected Boolean _IsPoligono = false;
+
+            public Poligonale()
             {
 
+            }
+
+            public Poligonale(List<Segmento> lineaSpezzata)
+            {
+                this.Manage(lineaSpezzata);
+            }
+
+            public Poligonale(List<Punto> punti)
+            {
+                this.Manage(punti);
+            }
+
+            private List<Punto> GetPunti(List<Segmento> lineaSpezzata)
+            {
+                List<Punto> ReturnValue = new List<Punto>();
+
+
+
+                return ReturnValue;
+            }
+
+            private List<Segmento> GetLineaSpezzataChiusa(List<Punto> lineaSpezzata)
+            {
+                List<Segmento> ReturnValue = new List<Segmento>();
+
+
+
+                return ReturnValue;
+            }
+
+            private void Manage(List<Punto> punti)
+            {
+
+            }
+
+            private void Manage(List<Segmento> lineaSpezzata)
+            {
+
+            }
+
+            public List<Punto> Punti
+            {
+                get { return this._Punti; }
+            }
+
+            public List<Segmento> LineaSpezzataChiusa
+            {
+                get { return this._LineaSpezzata; }
+            }
+
+            public  Boolean IsPoligono
+            {
+                get { return this._IsPoligono; }
+            }
+
+        }
+
+        public abstract class Poligono  
+        {
+            protected Poligonale _Poligonale = new Poligonale();
+
+            protected abstract Double GetArea();
+
+            public Poligono(Poligonale poligonale)
+            {
+
+            }
+
+
+
+            protected Double _Area = 0;
+
+            public Double Area
+            {
+                get { return this._Area; }
+            }
+
+            private Double _Perimetro = 0;
+
+            public Double Perimetro
+            {
+                get { return this._Perimetro; }
+            }
+
+            public int NumeroLati
+            {
+                get { return this._Poligonale.Punti.Count; }
             }
         }
 
---
+++ GeometriaSolution/Geometria/Poligoni.cs
@@ -0,0 +1,88 @@
+﻿using Geometria.ConcettoPrimitivo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometria.ConcettoPrimitivo.Poligoni
+{
+
+
+    //public class PoligonoRegolare : Poligono
+    //{
+    //    public PoligonoRegolare(List<Segmento> lati) : base(lati)
+    //    {
+
+    //    }
+
+    //    public PoligonoRegolare(List<Punto> punti) : base(punti)
+    //    {
+
+    //    }
+    //}
+
+    public class Triangolo : Poligono
+    {
+        public Triangolo(List<Segmento> lati) : base(lati)
+        {
+
+        }
+
+        public Triangolo(List<Punto> punti) : base(punti)
+        {
+
+        }
+
+        protected override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double GetPerimetro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TriangoloScaleno : Triangolo
+    {
+        public TriangoloScaleno(List<Segmento> lati) : base(lati)
+        {
+
+        }
+
+        public TriangoloScaleno(List<Punto> punti) : base(punti)
+        {
+
+        }
+    }
+
+    public class TriangoloIsoscele : Triangolo
+    {
+        public TriangoloIsoscele(List<Segmento> lati) : base(lati)
+        {
+
+        }
+
+        public TriangoloIsoscele(List<Punto> punti) : base(punti)
+        {
+
+        }
+    }
+
+    public class TriangoloEquilatero  : Triangolo
+    {
+        public TriangoloEquilatero(List<Segmento> lati) : base(lati)
+        {
+
+        }
+
+        public TriangoloEquilatero(List<Punto> punti) : base(punti)
+        {
+
+        }
+    }
+
+
+
+
+}
